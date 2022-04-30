1f81d1a70d4c9b9074ea1da091915dfb785d377e
saraakaram <sara.karam01@lau.edu>
2022-05-01 01:12:08 +0300

    final utils

--- utils.py
+++ utils.py
@@ -60,25 +60,11 @@ def dice(v1,v2):
     similarity=num/denom
     return similarity
 
-<<<<<<< HEAD
 def e_cosine(dimensions, v1 , v2):
-=======
-
-def e_cosine(dimensions, v1 , v2):
-    # terms = set(dimension.split(",")[0] for dimension in dimensions)
-    
-    # trm = 0
-    # tot = 0
-    # for term in terms:
-    #     for i in len(dimensions):
-    #         if term == dimensions[i].split(",")[0]:
-    #             trm += v1[i]*v2[i]*sim_context(dimensions.split(",")[1],dimensions.split(",")[1])
->>>>>>> a1e56cd34bcb30805550498e13d365b8eaab6064
     dict = {}
 
     for i in range(0, len(dimensions)):
         dimension = dimensions[i]
-<<<<<<< HEAD
         term = dimension.split(",")[0]
         context = dimension.split(",")[1]
 
@@ -142,11 +128,3 @@ def costDel(b):
 
 # print(sim_context("Department/Student","Department/Professor"))
 
-
-=======
-        if(dimension.split(",")[0] in dict):
-            dict[dimension.split(",")[0]][dimension.split(",")[1]] = i
-        else: dict[dimension.split(",")[0]] = { dimension.split(",")[1] : i }
-
-    print(dict)
->>>>>>> a1e56cd34bcb30805550498e13d365b8eaab6064
